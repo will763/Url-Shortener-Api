5b9c3430d33e9d20e1182c3ae9583b1b3aa649cd
will763 <killzasper@gmail.com>
2022-02-18 14:56:37 -0300

    add url constant

--- src/config/Constant.ts
+++ src/config/Constant.ts
@@ -1,4 +1,4 @@
 export const config = {
-    API_URL: "https://encurta",
+    API_URL: "https://url-shortener-api-ten.vercel.app",
     MONGO_CONNECTION : `mongodb+srv://${process.env.USER}:${process.env.PASSWORD}@cluster0.q24mx.mongodb.net/cluster0?retryWrites=true&w=majority`
 }
